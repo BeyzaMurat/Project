1db9f7696d585d3da08d522b699affb76bbbdf10
BeyzaMurat <121354275+BeyzaMurat@users.noreply.github.com>
2023-05-19 00:03:48 +0300

    Add files via upload

---
+++ Navbar/app.js
@@ -0,0 +1,10 @@
+const hamburger=document.querySelector(".hamburger");
+const navBar=document.querySelector(".nav-bar");
+
+
+hamburger.addEventListener("click",function(){
+navBar.classList.toggle("active");
+
+
+
+})
